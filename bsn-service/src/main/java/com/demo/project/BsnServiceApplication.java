3aa4ca9c5047525181ecaa6d2ac43b5f51d213e7
Hari Prasath Murugaraj <hariprasath.murugaraj@int.trustrace.com>
2024-08-26 18:38:20 +0530

    Backend Setup

---
+++ bsn-service/src/main/java/com/demo/project/BsnServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsnServiceApplication.class, args);
+	}
+
+}
